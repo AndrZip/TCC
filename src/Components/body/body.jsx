6d023936d029e411e413d4e2e6770ae2d52aa575
André <79728727+AndrZip@users.noreply.github.com>
2024-03-13 18:37:33 +0000

    sistema de colunas e linhas implantado para um possível alinhamento do componente "Body"

--- src/Components/body/body.jsx
+++ src/Components/body/body.jsx
@@ -5,12 +5,20 @@ const Body = () => {
     return (
         <>
             <div className="container">
-                <h1>Conectando corações, transformando vidas</h1>
-                <p>Unidos em um propósito: promover a adoção responsável e espalhar o amor incondicional pelos animais.</p>
-                <img src="https://i.stack.imgur.com/hRup4.jpg?s=256&g=1" className="img-fluid" alt="imagem sla" />
-            </div>
-            <div className="container">
-                <img src="https://i.stack.imgur.com/IBHo9.jpg?s=256&g=1" className="img-fluid" alt="imagem sla 2" />
+                <div className="col-md">
+                    <div className="row">
+                        <h1>Conectando corações, transformando vidas</h1>
+                        <p>Unidos em um propósito: promover a adoção responsável e espalhar o amor incondicional pelos animais.</p>
+                    </div>
+                    <div className="row">
+                        <img src="https://i.stack.imgur.com/hRup4.jpg?s=256&g=1" className="img-fluid" alt="imagem sla" />
+                    </div>
+                </div>
+                <div className="col-md">
+                    <div className="row">
+                        <img src="https://i.stack.imgur.com/hRup4.jpg?s=256&g=1" className="img-fluid" alt="imagem sla 2" />
+                    </div>
+                </div>
             </div>
         </>
     )
