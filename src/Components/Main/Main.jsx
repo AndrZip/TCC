da88e0e50f5f78a6c7ed489ba1d7f69e91cba48d
AndrZip <andrewemail52@gmail.com>
2024-03-24 17:12:12 -0300

    #

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,18 +8,15 @@ const Main = () => {
         <>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col">
-                        <h1>Conectando corações, transformando vidas</h1>
-                        <p>Unidos em um propósito: promover a adoção responsável e espalhar o amor incondicional pelos animais.</p>
+                    <div className="col coluna1">
+                        <h1 className="display-3">Conectando corações, transformando vidas</h1>
+                        <p className="lead">Unidos em um propósito: promover a adoção responsável e espalhar o amor incondicional pelos animais.</p>
                         <Btn type="primary" size="lg" text="Quero adotar" />
                         <Btn type="secondary" size="lg" text="Quero doar" />
-
-                        <div className="row mt-5">
-                            <img src="https://portal.cnpae.embrapa.br/vitrine/wp-content/uploads/sites/3/2021/10/topo_torta_pinhao.jpg" className="img-fluid" id="parceria" alt="parceria" />
-                        </div>
+                        <img src="https://portal.cnpae.embrapa.br/vitrine/wp-content/uploads/sites/3/2021/10/topo_torta_pinhao.jpg" className="img-fluid rounded" alt="parceria" />
                     </div>
-                    <div className="col">
-                        <img src="https://alconpet.com.br/common/img/produto/produto_lg/alcon-basic.webp" className="img-fluid" alt="banner" />
+                    <div className="col coluna2">
+                        <img src="https://alconpet.com.br/common/img/produto/produto_lg/alcon-basic.webp" className="img-fluid rounded" alt="banner" />
                     </div>
                 </div>
             </div>
